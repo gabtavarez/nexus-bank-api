9f1b531b4f7e751b15cf847225395275e45c2d9e
gabtavarez <gabrieltavarescs03@gmail.com>
2026-04-23 23:05:55 -0300

    implement UserService with email duplication check

--- src/main/java/com/tavarez/nexusbank/repositories/UserRepository.java
+++ src/main/java/com/tavarez/nexusbank/repositories/UserRepository.java
@@ -2,6 +2,9 @@ package com.tavarez.nexusbank.repositories;
 
 import com.tavarez.nexusbank.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
 }
--- src/main/java/com/tavarez/nexusbank/services/UserService.java
+++ src/main/java/com/tavarez/nexusbank/services/UserService.java
@@ -1,4 +1,38 @@
 package com.tavarez.nexusbank.services;
 
+import com.tavarez.nexusbank.dto.request.UserRequestDTO;
+import com.tavarez.nexusbank.dto.response.UserResponseDTO;
+import com.tavarez.nexusbank.entities.User;
+import com.tavarez.nexusbank.exceptions.BusinessException;
+import com.tavarez.nexusbank.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Transactional
+    public UserResponseDTO create(UserRequestDTO dto) {
+        if (userRepository.existsByEmail(dto.email())) {
+            throw new BusinessException("Email já cadastrado no sistema.");
+        }
+
+        User user = User.builder()
+                .username(dto.username())
+                .email(dto.email())
+                .password(dto.password()) // No futuro aplicaremos o Hash aqui
+                .build();
+
+        user = userRepository.save(user);
+
+        return new UserResponseDTO(
+                user.getId(),
+                user.getUsername(),
+                user.getEmail()
+        );
+    }
 }
